b2dca05e6134a3b07f328e8f5a3c5cd5f02f2d6b
vilmar2025 <vilmarpereiradesouzafilho@gmail.com>
2025-10-28 18:19:21 -0300

    first commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/site-iphone-17/',
-})
+  base: "/iphone.17",
+});
